a9ce3f7d37ab4fab9673762eb3adc1f8cf6c44b0
vivianix28 <viviana.farese@studio.unibo.it>
2024-08-14 18:01:40 +0200

    Create flock.hpp

---
+++ flock.hpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include "boids.hpp"
+
+#ifndef FLOCK_HPP
+#define FLOCK_HPP
+
+namespace boids {
+class Flock {
+    private:
+    std::vector<Boid> flock;
+
+    public:
+    Flock() {} // constructor
+
+    int getSize();
+    Boid &getBoid(int i);
+    void addBoid(const Boid& b);
+    void flocking();
+};
+}
+
+#endif 
