bfe0ed08899b1304c4f7c246e329e1db23b61baa
vivianix28 <viviana.farese@studio.unibo.it>
2024-08-14 18:00:03 +0200

    Update boids.cpp

--- boids.cpp
+++ boids.cpp
@@ -1,53 +1,147 @@
 #include "boids.hpp"
 
-namespace boids {
-      Vec2 Vec2::operator+(const Vec2& other) const {
-    return Vec2(x + other.x, y + other.y);
-  }
+#include "SFML/Graphics.hpp"
 
-  Vec2 Vec2::operator-(const Vec2& other) const {
-    return Vec2(x - other.x, y - other.y);
-  }
+namespace boids {
+
+double perception = 50;  // field of vision
+
+Vec2 Vec2::operator+(const Vec2& other) const {
+  return Vec2(x_ + other.x_, y_ + other.y_);
+}
+
+Vec2 Vec2::operator-(const Vec2& other) const {
+  return Vec2(x_ - other.x_, y_ - other.y_);
+}
 
 Vec2& Vec2::operator+=(const Vec2& other) {
-    x += other.x;
-    y += other.y;
-    return *this;
-  }
+  x_ += other.x_;
+  y_ += other.y_;
+  return *this;
+}
 
-    Vec2& Vec2::operator-=(const Vec2& other) {
-    x -= other.x;
-    y -= other.y;
-    return *this;
-  }
+Vec2& Vec2::operator-=(const Vec2& other) {
+  x_ -= other.x_;
+  y_ -= other.y_;
+  return *this;
+}
 
-  Vec2 Vec2::operator*(double scalar) const { return Vec2(x * scalar, y * scalar); }
+Vec2 Vec2::operator*(double scalar) const {
+  return Vec2(x_ * scalar, y_ * scalar);
+}
 
-  double Vec2::lenght() const { return std::sqrt(x * x + y * y); }
-class Boid {
-public:
-    Vec2 position;
-    Vec2 velocity;
+Vec2& Vec2::operator*=(double scalar) {
+  x_ *= x_ * scalar;
+  y_ *= y_ * scalar;
+  return *this;
+}
 
-    /*Boid(double x, double y) : position{x, y}, velocity{0, 0} {}*/
-private:
-    Vec2 calculateCohesion(const std::vector<Boid>& boids, float cohesionFactor) {
-        Vec2 centerOfMass(0, 0);
-        int count = 0;
+Vec2 Vec2::operator/(double scalar) const {
+  return Vec2(x_ / scalar, y_ / scalar);
+}
 
-        for (const auto& boid : boids) {
-            if (&boid != this) {  // Confronta l'indirizzo dell'oggetto
-                centerOfMass += boid.position;
-                count++;
-            }
-        }
+double Vec2::lenght() const { return std::sqrt(x_ * x_ + y_ * y_); }
 
-   if (count > 0) {
-            centerOfMass = centerOfMass / count;  // Calcola il centro di massa
-        }
+double Vec2::distance(const Vec2& other) {
+  double dx_ = x_ - other.x_;
+  double dy_ = y_ - other.y_;
+  double dist = std::sqrt(dx_ * dx_ + dy_ * dy_);
+  return dist;
+}
 
-        Vec2 cohesionVelocity = (centerOfMass - this->position) * cohesionFactor;
-        return cohesionVelocity;
+Boid::Boid(double x_, double y_) {
+  position_ = Vec2(x_, y_);
+  velocity_ = Vec2(rand() % 3, rand() % 3);
+  acceleration_ = Vec2(0, 0);
+}
+
+Vec2 Boid::setPosition(double x_, double y_) {
+  position_.x_ = x_;
+  position_.y_ = y_;
+  return position_;
+}
+
+std::vector<Boid> Boid::getNeighbors(const std::vector<Boid>& boids) {
+    std::vector<Boid> neighbors;
+
+  for (int i = 0; i < boids.size();
+       ++i) {  // calculating respective distance of boids
+    double d = position_.distance(boids[i].position_);
+    if ((d > 0) && (d < perception)) {
+      neighbors.push_back(boids[i]);
     }
+  }
+  return neighbors;
 }
+
+Vec2 Boid::alignment(const std::vector<Boid>& boids, double a) {
+  auto neighbors = getNeighbors(boids);
+  Vec2 steer;
+  Vec2 sum(0,0);
+
+  for (int i = 0; i < neighbors.size();
+       ++i) {  
+     sum += neighbors[i].velocity_;
+    }
+
+  if (neighbors.size() > 0) {
+    // dividing by number of neighbor boids to
+    // obtain average velocity_
+    steer = ((sum / neighbors.size()) - velocity_) * a;  // a is the alignment factor
+  }
+
+  return steer;
+};
+
+Vec2 Boid::cohesion(const std::vector<Boid>& boids, float cohesionFactor) {
+  Vec2 centerOfMass(0, 0);
+  int count = 0;
+
+  for (const auto& boid : boids) {
+    if (&boid != this) {  // Confronta l'indirizzo dell'oggetto
+      centerOfMass += boid.position_;
+      count++;
+    }
+  }
+
+  if (count > 0) {
+    centerOfMass = centerOfMass / count;  // Calcola il centro di massa
+  }
+
+  Vec2 cohesionVelocity = (centerOfMass - this->position_) * cohesionFactor;
+  return cohesionVelocity;
 }
+
+void Boid::update(const std::vector<Boid>& v, double a) {
+  Vec2 ali = alignment(v, 0.6);
+  Vec2 cohe = cohesion(v, 0.001);
+  acceleration_ += ali;
+  acceleration_ += cohe;
+  velocity_ += acceleration_;
+  position_ += velocity_;
+  acceleration_ *= 0;
+}
+
+// sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+
+void Boid::borders() {
+  if (position_.x_ < 0) {
+    position_.x_ += 810;
+  }
+  if (position_.x_ > 800) {
+    position_.x_ -= 810;
+  }
+  if (position_.y_ < 0) {
+    position_.y_ += 610;
+  }
+  if (position_.y_ > 600) {
+    position_.y_ -= 610;
+  }
+}
+
+void Boid::run(const std::vector<Boid>& v, double a) {
+  update(v, a);
+  borders();
+}
+
+}  // namespace boids
