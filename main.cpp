1b6db8f280502c8e717d9af64a46016e4379dcd3
vivianix28 <viviana.farese@studio.unibo.it>
2024-08-28 18:26:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,64 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <iostream>
-#include <vector>
-
 #include "boids.hpp"
 #include "flock.hpp"
+#include "slider.hpp"
+
+#include <ctime>
+#include <iostream>
+#include <limits>
+
+int numberOfBoids() {
+  std::cout << "insert desired (integer) number of boids between 2 and "
+               "700:\n -numbers "
+               "between 300 and 450 are recommended for a better performance "
+               "\n -decimal numbers or numbers followed by letters will be rounded to first integer figure"
+            << '\n';
+  int n;
+  while (true) {
+    std::cin >> n;
+    if (n >= 2 && n <= 700) {
+      return n;
+    } else {
+      std::cout << "invalid input, try again." << '\n';
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+}
 
 int main() {
-  // biulding the window through constructor
-  sf::RenderWindow window(
-      sf::VideoMode(800, 600), "simulation",
-      sf::Style::Titlebar);  // video mode defines the size of the window
+  sf::RenderWindow window(sf::VideoMode(800, 600), "simulation",
+                          sf::Style::Titlebar);
   window.setPosition(
-      sf::Vector2i(10, 50));  // window appears in the corner of the screen
-                              //  window.setVerticalSyncEnabled(true);
+      sf::Vector2i(10, 50));
   window.setFramerateLimit(60);
-
-  sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-
-  sf::RectangleShape sliderBar(sf::Vector2f(600, 10));
-  sliderBar.setPosition(100, 100);
-  sliderBar.setFillColor(sf::Color::White);
-
-  sf::CircleShape sliderKnob(10);
-  sliderKnob.setFillColor(sf::Color::Red);
-  sliderKnob.setPosition(100, 95);
-
-  bool isDragging = false;
-  double a = 0;
 
   boids::Flock flock;
   std::vector<sf::CircleShape> shapes;
 
-  for (int i = 0; i < 100; ++i) {
-    boids::Boid b(rand() % 800, rand() % 600);
-    sf::CircleShape shape(8, 3);
-    shape.setPosition(desktop.width, desktop.height);
+  Slider alignmentSlider(0, 1, 0, "alignment");
+  alignmentSlider.setPosition(100, 100);
+
+  Slider cohesionSlider(0, 0.005, 0, "cohesion");
+  cohesionSlider.setPosition(100, 150);
+
+  Slider separationSlider(0, 5, 0, "separation");
+  separationSlider.setPosition(100, 200);
+
+  Slider perceptionSlider(0, 60, 0, "perception");
+  perceptionSlider.setPosition(100, 250);
+
+  Slider sepDistanceSlider(0, 30, 0, "separation distance");
+  sepDistanceSlider.setPosition(100, 300);
+
+  srand(static_cast<unsigned int>(time(0)));
+
+  int n { numberOfBoids() };
+
+  for (int i = 0; i < n; ++i) {
+    boids::Boid b(rand() % 800, rand() % 600, rand() % 3, rand() % 3);
+    sf::CircleShape shape(5);
+    shape.setPosition(static_cast<float>(b.getPosition().x_),
+                      static_cast<float>(b.getPosition().y_));
     flock.addBoid(b);
     shapes.push_back(shape);
   }
@@ -47,57 +70,47 @@ int main() {
         window.close();
       }
 
-      if (event.type == sf::Event::MouseButtonPressed) {
-        if (sliderKnob.getGlobalBounds().contains(event.mouseButton.x,
-                                                  event.mouseButton.y)) {
-          isDragging = true;
-        }
-      }
-
-      if (event.type == sf::Event::MouseButtonReleased) {
-        isDragging = false;
-      }
-
-      if (event.type == sf::Event::MouseMoved && isDragging) {
-        float newPosX = event.mouseMove.x - sliderKnob.getRadius();
-        newPosX = std::max(100.0f, std::min(newPosX, 690.0f));
-        sliderKnob.setPosition(newPosX, sliderKnob.getPosition().y);
-
-        // Mappare la posizione del knob a un valore tra 0 e 2
-        a = ((newPosX - 100.0f) / 590.0f) * 2.0f;
-      }
+      alignmentSlider.handleEvent(event);
+      cohesionSlider.handleEvent(event);
+      separationSlider.handleEvent(event);
+      perceptionSlider.handleEvent(event);
+      sepDistanceSlider.handleEvent(event);
     }
 
-    window.clear(sf::Color::Black);  // always call clear before draw
+    double a { alignmentSlider.getValue() };
+    double c { cohesionSlider.getValue() };
+    double s { separationSlider.getValue() };
+    double d { perceptionSlider.getValue() };
+    double ds { sepDistanceSlider.getValue() };
 
-    for (int i = 0; i < shapes.size(); ++i) {
+    window.clear(sf::Color::Black);
+
+    for (long unsigned int i = 0; i < shapes.size(); ++i) {
       window.draw(shapes[i]);
-      shapes[i].setPosition(flock.getBoid(i).getPosition().x_,
-                            flock.getBoid(i).getPosition().y_);
+      shapes[i].setPosition(
+          static_cast<float>(flock.getBoid(i).getPosition().x_),
+          static_cast<float>(flock.getBoid(i).getPosition().y_));
     }
 
-    flock.flocking();
-    window.draw(sliderBar);
-    window.draw(sliderKnob);
+    flock.flocking(a, c, s, d, ds);
+    alignmentSlider.render(window);
+    cohesionSlider.render(window);
+    separationSlider.render(window);
+    perceptionSlider.render(window);
+    sepDistanceSlider.render(window);
     window.display();
-    while (window.isOpen()) {
-    // Gestione degli eventi
 
     // Calcolo delle statistiche
-    double averageDistance = flock.calculateAverageDistance();
-    double averageSpeed = flock.calculateAverageSpeed();
-    double distanceStdDev = flock.calculateDistanceStandardDeviation();
-    double speedStdDev = flock.calculateSpeedStandardDeviation();
+    double averageDistance { flock.calculateAverageDistance() };
+    double averageSpeed { flock.calculateAverageSpeed() };
+    double distanceStdDev { flock.calculateDistanceStandardDeviation() };
+    double speedStdDev { flock.calculateSpeedStandardDeviation() };
 
     // Visualizzazione delle statistiche sulla console
-    std::cout << "Average Distance: " << averageDistance << "\n";
-    std::cout << "Average Speed: " << averageSpeed << "\n";
-    std::cout << "Distance Standard Deviation: " << distanceStdDev << "\n";
-    std::cout << "Speed Standard Deviation: " << speedStdDev << "\n";
-
-
-}
-
+    std::cout << "\rAverage Distance: " << averageDistance << '\n';
+    std::cout << "\rAverage Speed: " << averageSpeed << '\n';
+    std::cout << "\rDistance Standard Deviation: " << distanceStdDev << '\n';
+    std::cout << "\rSpeed Standard Deviation: " << speedStdDev << '\n';
   }
   return 0;
 }
