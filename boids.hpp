9dd8a4b38ac457515d5785711926a0f5d8e9b5e8
vivianix28 <viviana.farese@studio.unibo.it>
2024-08-28 18:27:39 +0200

    Update boids.hpp

--- boids.hpp
+++ boids.hpp
@@ -1,19 +1,17 @@
 #ifndef BOIDS_HPP
 #define BOIDS_HPP
 
-#include <cmath>
-#include <iostream>
-#include <random>
 #include <vector>
 
 namespace boids {
-extern double a;
 
-struct Vec2 {  // vettore in due dimensioni
+struct Vec2 {
   double x_;
   double y_;
   Vec2() : x_(0), y_(0) {}
   Vec2(double x, double y) : x_(x), y_(y) {}
+
+  void setValues(double x, double y);
 
   Vec2 operator+(const Vec2& other) const;
 
@@ -21,7 +19,7 @@ struct Vec2 {  // vettore in due dimensioni
 
   Vec2& operator+=(const Vec2& other);
 
-  Vec2& operator-=(const Vec2& other);
+//  Vec2& operator-=(const Vec2& other);
 
   Vec2 operator*(double scalar) const;
 
@@ -29,9 +27,14 @@ struct Vec2 {  // vettore in due dimensioni
 
   Vec2 operator/(double scalar) const;
 
+//  bool operator==(const Vec2& other) const;
+
   double lenght() const;
 
-  double distance(const Vec2& other);
+  void limit(double max);
+
+  double distance(const Vec2& other) const;
+
   Vec2 normalize() const;
 };
 
@@ -40,30 +43,31 @@ class Boid {
   Vec2 position_;
   Vec2 velocity_;
   Vec2 acceleration_;
-
+  double maxSpeed;
 
  public:
   Boid() {}
-  Boid(double x, double y);
+  Boid(double x, double y, double vx, double vy);
 
-  Vec2 getPosition() { return position_; }
-  Vec2 getVelocity() { return velocity_; }
-  Vec2 getAcceleration() { return acceleration_; }
-
+  Vec2 getPosition() const { return position_; }
+  Vec2 getVelocity() const { return velocity_; }
+  Vec2 getAcceleration() const { return acceleration_; }
 
   Vec2 setPosition(double x, double y);
 
-  std::vector<Boid> getNeighbors(const std::vector<Boid>& boids);
+  std::vector<Boid> getNeighbors(const std::vector<Boid>& boids, double perception) const;
 
   // funzioni delle tre regole
-  Vec2 alignment(const std::vector<Boid>& boids, double a);
-  Vec2 cohesion(const std::vector<Boid>& boids, float cohesionFactor);
+  Vec2 alignment(const std::vector<Boid>& boids, double alignmentFactor, double perception) const;
+  Vec2 cohesion(const std::vector<Boid>& boids, double cohesionFactor, double perception) const;
+  Vec2 separation(const std::vector<Boid>& boids, double separationDistance,
+                  double separationFactor, double perception) const;
 
-  void update(const std::vector<Boid>& v, double a);
+  void update(const std::vector<Boid>& v, double a, double c, double s, double d, double ds);
 
   void borders();
 
-  void run(const std::vector<Boid>& v, double a);
+  void run(const std::vector<Boid>& v, double a, double c, double s, double d, double ds);
 };
 };  // namespace boids
 
