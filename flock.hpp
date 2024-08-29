522af999c6644be29a0f06be0c98fb1194359fe1
vivianix28 <viviana.farese@studio.unibo.it>
2024-08-29 10:58:32 +0200

    Update flock.hpp

--- flock.hpp
+++ flock.hpp
@@ -1,27 +1,26 @@
-#include <iostream>
-#include <vector>
-#include "boids.hpp"
-
 #ifndef FLOCK_HPP
 #define FLOCK_HPP
 
+#include "boids.hpp"
+
+
 namespace boids {
 class Flock {
-    private:
-    std::vector<Boid> flock;
+ private:
+  std::vector<Boid> flock;
 
-    public:
-    Flock() {} // constructor
+ public:
+  Flock() {}
 
-    int getSize();
-    Boid &getBoid(int i);
-    void addBoid(const Boid& b);
-    void flocking();
-    double calculateAverageSpeed() const;
-    double calculateAverageDistance() const;
-    double calculateSpeedStandardDeviation() const;
-    double alculateDistanceStandardDeviation() const;
+  long unsigned int getSize() const;
+  const Boid& getBoid(long unsigned int i) const;
+  void addBoid(const Boid& b);
+  void flocking(double a, double c, double s, double d, double ds);
+  double calculateAverageSpeed() const;
+  double calculateAverageDistance() const;
+  double calculateSpeedStandardDeviation() const;
+  double calculateDistanceStandardDeviation() const;
 };
-}
+}  // namespace boids
 
-#endif 
+#endif
